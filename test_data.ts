6e96eb005de7b152d01468bedb3e78692e9f9a2c
Reed McDaniel <reed.mcdaniel.716@gmail.com>
2023-12-02 14:11:28 -0500

    all test data

--- test_data.ts
+++ test_data.ts
@@ -6,8 +6,10 @@ const {
   productDescriptor,
 } = require("./types.ts");
 
+// USERS
+const userId1 = faker.string.uuid();
 const user1 = {
-  id: faker.string.uuid(),
+  id: userId1,
   username: faker.internet.userName(),
   password: faker.internet.password(),
   created_at: faker.date.future(),
@@ -16,8 +18,9 @@ const user1 = {
 
 console.log(JSON.stringify(user1));
 
+const userId2 = faker.string.uuid();
 const user2 = {
-  id: faker.string.uuid(),
+  id: userId2,
   username: faker.internet.userName(),
   password: faker.internet.password(),
   created_at: faker.date.future(),
@@ -26,11 +29,13 @@ const user2 = {
 
 console.log(JSON.stringify(user2));
 
+// PRODUCTS
 const productBrand1 = faker.helpers.enumValue(productBrands);
 const productColor1 = faker.helpers.enumValue(productColors);
+const productId1 = faker.string.uuid();
 
 const product1 = {
-  id: faker.string.uuid(),
+  id: productId1,
   name: `${productBrand1} ${faker.helpers.enumValue(productDescriptor)} Gloves`,
   size: "16oz",
   brand: productBrand1,
@@ -42,9 +47,10 @@ console.log(JSON.stringify(product1));
 
 const productBrand2 = faker.helpers.enumValue(productBrands);
 const productColor2 = faker.helpers.enumValue(productColors);
+const productId2 = faker.string.uuid();
 
 const product2 = {
-  id: faker.string.uuid(),
+  id: productId2,
   name: `${productBrand2} ${faker.helpers.enumValue(productDescriptor)} Shorts`,
   size: "xxl",
   brand: productBrand2,
@@ -53,3 +59,53 @@ const product2 = {
 };
 
 console.log(JSON.stringify(product2));
+
+// CARTS
+
+const cartId1 = faker.string.uuid();
+
+const cart1 = {
+  id: cartId1,
+  user_id: userId1,
+  product_id: productId1,
+  quantity: 1,
+};
+
+console.log(JSON.stringify(cart1));
+
+const cartId2 = faker.string.uuid();
+
+const cart2 = {
+  id: cartId2,
+  user_id: userId1,
+  product_id: productId2,
+  quantity: 2,
+};
+
+console.log(JSON.stringify(cart2));
+
+// ORDERS
+
+const orderId1 = faker.string.uuid();
+
+const order1 = {
+  id: orderId1,
+  user_id: userId1,
+  cart_id: cartId1,
+  order_date: faker.date.future(),
+  is_gift: faker.datatype.boolean(),
+};
+
+console.log(JSON.stringify(order1));
+
+const orderId2 = faker.string.uuid();
+
+const order2 = {
+  id: orderId2,
+  user_id: userId2,
+  cart_id: cartId2,
+  order_date: faker.date.future(),
+  is_gift: faker.datatype.boolean(),
+};
+
+console.log(JSON.stringify(order2));
